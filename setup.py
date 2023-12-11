e043b9eded95202745d4db2b7f70fa040bb87eb8
lihaowei <646300129@qq.com>
2023-12-11 15:46:15 +0800

    修改包名问题导致发布失败

--- setup.py
+++ setup.py
@@ -1,20 +1,5 @@
-from os.path import dirname
-from os.path import join
-import setuptools
-
-
-def readme() -> str:
-    """Utility function to read the README file.
-    Used for the long_description.  It's nice, because now 1) we have a top
-    level README file and 2) it's easier to type in the README file than to put
-    a raw string in below.
-    :return: content of README.md
-    """
-    return open(join(dirname(__file__), "README.md")).read()
-
-
 setuptools.setup(
-    name="lihaowei/streamlit-echarts",
+    name="streamlit_echarts_lihaowei",
     version="0.1.0",
     author="lihaowei",
     author_email="646300129@qq.com",
